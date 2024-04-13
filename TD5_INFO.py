8cf5672d12090b8ca817849475d06f6a71e3b2e7
Nino Jouniaux <ninojouniaux@MacBook-Air-de-Nino.local>
2024-04-13 17:58:06 +0200

    Fichier TD5 V2

--- TD5_INFO.py
+++ TD5_INFO.py
@@ -8,12 +8,12 @@ w.title("Cible")
 can = Canvas(w,width=400,height=400,bg="red") #can = canvas
 can.grid(row=0,column=0,columnspan=3)
 #cercles
-can.create_oval(30,30,370,370, outline="red",fill="white")
-can.create_oval(60,60,340,340,outline="red",fill="white")
-can.create_oval(90,90,310,310,outline="red",fill="white")
-can.create_oval(120,120,280,280,outline="red",fill="white")
+can.create_oval(30,30,370,370, outline="red",fill="ivory")
+can.create_oval(60,60,340,340,outline="red",fill="ivory")
+can.create_oval(90,90,310,310,outline="red",fill="ivory")
+can.create_oval(120,120,280,280,outline="red",fill="ivory")
 can.create_oval(150,150,250,250,outline="red",fill="red")
-can.create_oval(180,180,220,220,outline="red",fill="white")
+can.create_oval(180,180,220,220,outline="red",fill="ivory")
 #lignes
 can.create_line(200,0,200,400,fill="red")
 can.create_line(0,200,400,200,fill="red")
@@ -22,7 +22,7 @@ can.create_text(200,45,text='1',font=('Times','12','bold'),fill="red")
 can.create_text(200,75,text='2',font=('Times','12','bold'),fill="red")
 can.create_text(200,105,text='3',font=('Times','12','bold'),fill="red")
 can.create_text(200,135,text='4',font=('Times','12','bold'),fill="red")
-can.create_text(200,165,text='5',font=('Times','12','bold'),fill="white")
+can.create_text(200,165,text='5',font=('Times','12','bold'),fill="ivory")
 can.create_text(200,195,text='6',font=('Times','12','bold'),fill="red")
 #boutons
 b1=Button(w)
@@ -38,37 +38,40 @@ b2["command"]=w.destroy
 Compteurglobal = 0
 
 def tir():
+    global a
     global Compteurglobal
     X=[]
     Y=[]
     compteur=0
-    for i in range(5):
-        tiragex=random.randint(8,392)
-        tiragey=random.randint(8,392)
+    for i in range(5-a):
+        tiragex=random.randint(0,400)
+        tiragey=random.randint(0,400)
         X.append(tiragex) # on conserve les coordonnées du point pour calculer les scores par la suite
         Y.append(tiragey)
         can.create_oval(tiragex-8,tiragey-8,tiragex+8,tiragey+8, outline="red",fill="black") #créé le cercle du à l'impact
-        if (X[i]-200)**2+(Y[i]-200)**2<=30**2:
+        if (X[i]-200)**2+(Y[i]-200)**2<=15**2:
             compteur+=6
-        elif (X[i]-200)**2+(Y[i]-200)**2<=60**2:
+        elif (X[i]-200)**2+(Y[i]-200)**2<=45**2:
             compteur+=5
-        elif (X[i]-200)**2+(Y[i]-200)**2<=90**2:
+        elif (X[i]-200)**2+(Y[i]-200)**2<=75**2:
             compteur+=4
-        elif (X[i]-200)**2+(Y[i]-200)**2<=120**2:
+        elif (X[i]-200)**2+(Y[i]-200)**2<=105**2:
             compteur+=3
-        elif (X[i]-200)**2+(Y[i]-200)**2<=150**2:
+        elif (X[i]-200)**2+(Y[i]-200)**2<=135**2:
             compteur+=2
-        elif (X[i]-200)**2+(Y[i]-200)**2<=180**2:
+        elif (X[i]-200)**2+(Y[i]-200)**2<=165**2:
             compteur+=1       
     Compteurglobal+=compteur
-    lab=tkinter.Label(w,text=f"Le score est {Compteurglobal} ")
+    lab=tkinter.Label(w,text=f"Score : {Compteurglobal} ")
     lab.grid(row=3,column=1,sticky=tkinter.W)
+    a = 0
 b1["command"]=tir  
-
+a = 0  # cette variable sert à ce que quand on tire 2 fois à l'aide de "f", on tire que 3 fois si on appuie sur "feu" etc...
 def tirunique(event):
     global Compteurglobal
-    tiragex=random.randint(8,392)
-    tiragey=random.randint(8,392)
+    global a
+    tiragex=random.randint(0,400)
+    tiragey=random.randint(0,400)
     can.create_oval(tiragex-8,tiragey-8,tiragex+8,tiragey+8, outline="red",fill="black")
     if (tiragex-200)**2+(tiragey-200)**2<=30**2:
             Compteurglobal+=6
@@ -82,8 +85,9 @@ def tirunique(event):
             Compteurglobal+=2
     elif (tiragex-200)**2+(tiragey-200)**2<=180**2:
             Compteurglobal+=1
-    lab=tkinter.Label(w,text=f"Le score est {Compteurglobal} ")
+    lab=tkinter.Label(w,text=f"Score : {Compteurglobal} ")
     lab.grid(row=3,column=1,sticky=tkinter.W)
+    a = a + 1
 
 w.bind("<f>",tirunique)
 
